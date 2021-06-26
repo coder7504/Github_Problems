769d977c6466709c875cf8242bf34d7a225a405d
coder7504 <asliddin2703@gmail.com>
2021-06-26 18:00:47 +0500

    Initial Commit

---
+++ Github_Problems/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Github_Problems
+//
+//  Created by Asliddin Mahmudov on 6/26/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
